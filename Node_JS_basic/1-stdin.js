cdc23b43d353d69736d45230499a4569d2816884
Sebas-crypt <usefullseba17@gmail.com>
2024-05-28 16:48:48 -0400

    adding tasks 2 to 5 and fixing task 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,14 @@
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.setEncoding('utf8');
 
-process.stdin.on('data', (data) => {
-  console.log(`Your name is: ${data}`);
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
 });
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
