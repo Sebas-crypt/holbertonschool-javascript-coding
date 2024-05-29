e29e454a3420bfc2156e60e4e0025ad5f6b6d5b9
Sebas-crypt <usefullseba17@gmail.com>
2024-05-29 10:54:20 -0400

    fixing task 7

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,57 +1,28 @@
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
+
+const args = process.argv.slice(2);
+const countStudents = require('./3-read_file_async');
+
+const DATABASE = args[0];
 
 const app = express();
 const port = 1245;
-
-// Helper function to read CSV file and parse students
-function readDatabase(filePath) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(filePath, 'utf8', (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        const lines = data.split('\n').filter(line => line.trim() !== '');
-        const students = {};
-        lines.forEach((line, index) => {
-          if (index > 0) {
-            const [firstName, field] = line.split(',');
-            if (field) {
-              if (!students[field]) {
-                students[field] = [];
-              }
-              students[field].push(firstName);
-            }
-          }
-        });
-        resolve(students);
-      }
-    });
-  });
-}
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
 app.get('/students', async (req, res) => {
-  const database = process.argv[2];
+  const msg = 'This is the list of our students\n';
   try {
-    const students = await readDatabase(database);
-    let responseText = 'This is the list of our students\n';
-    const fields = Object.keys(students);
-    fields.forEach(field => {
-      responseText += `Number of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}\n`;
-    });
-    res.send(responseText.trim());
+    const students = await countStudents(DATABASE);
+    res.send(`${msg}${students.join('\n')}`);
   } catch (error) {
-    res.send('Cannot load the database');
+    res.send(`${msg}${error.message}`);
   }
 });
 
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
 });
 
 module.exports = app;
